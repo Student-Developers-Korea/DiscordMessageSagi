c10a0934442997b0f88b550ee20cd7311ba7d3a4
ditto7890 <59792804+ditto7890@users.noreply.github.com>
2020-08-15 20:51:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-function getMessage(id) {
-    return $('#app-mount .app-1q1i1E div').childNodes[1].firstElementChild.firstElementChild.lastElementChild.firstElementChild.childNodes[1].lastElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.childNodes[id].firstElementChild.lastElementChild.textContent
-}
-
-function editMessage(id, content) {
-    $('#app-mount .app-1q1i1E div').childNodes[1].firstElementChild.firstElementChild.lastElementChild.firstElementChild.childNodes[1].lastElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.childNodes[id].firstElementChild.lastElementChild.innerText = content.toString()
-    return getMessage(id)
-}
-
-function fetchMessage() {
-    var messages = {}
-    $('#app-mount .app-1q1i1E div').childNodes[1].firstElementChild.firstElementChild.lastElementChild.firstElementChild.childNodes[1].lastElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.childNodes.forEach(x => {
-        if (!x.id.includes('messages')) return
-        if (!x.firstElementChild) return
-        messages[x.id.split('messages-')[1]] = x.firstElementChild.lastElementChild.textContent
-    })
-    return messages
+const messages = {
+    get: id => document.querySelector('#app-mount .app-1q1i1E div').childNodes[1].firstElementChild.firstElementChild.lastElementChild.firstElementChild.childNodes[1].lastElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.childNodes[id].firstElementChild.lastElementChild.textContent,
+    edit: (id, content) => {
+        document.querySelector('#app-mount .app-1q1i1E div').childNodes[1].firstElementChild.firstElementChild.lastElementChild.firstElementChild.childNodes[1].lastElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.childNodes[id].firstElementChild.lastElementChild.innerText = content
+        return getMessage(id)
+    },
+    fetch: () => {
+        var messages = {}
+        
+        document.querySelector('#app-mount .app-1q1i1E div').childNodes[1].firstElementChild.firstElementChild.lastElementChild.firstElementChild.childNodes[1].lastElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.childNodes.forEach(x => {
+            if (!x.id.includes('messages')) return
+            if (!x.firstElementChild) return
+            messages[x.id.split('messages-')[1]] = x.firstElementChild.lastElementChild.textContent
+        })
+        
+        return messages
+    }
 }
