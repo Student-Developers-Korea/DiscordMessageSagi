7c4794e24856bc9e4b2c0eafad4a8ed01d80c0b5
ditto7890 <59792804+ditto7890@users.noreply.github.com>
2020-08-15 20:57:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const messages = {
+const message = {
+    nodeList: () => document.querySelector('#app-mount .app-1q1i1E div').childNodes[1].firstElementChild.firstElementChild.lastElementChild.firstElementChild.childNodes[1].lastElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.childNodes,
     get: id => document.querySelector('#app-mount .app-1q1i1E div').childNodes[1].firstElementChild.firstElementChild.lastElementChild.firstElementChild.childNodes[1].lastElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.childNodes[id].firstElementChild.lastElementChild.textContent,
     edit: (id, content) => {
         document.querySelector('#app-mount .app-1q1i1E div').childNodes[1].firstElementChild.firstElementChild.lastElementChild.firstElementChild.childNodes[1].lastElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.childNodes[id].firstElementChild.lastElementChild.innerText = content
-        return getMessage(id)
+        return message.get(id)
     },
     fetch: () => {
         var messages = {}
